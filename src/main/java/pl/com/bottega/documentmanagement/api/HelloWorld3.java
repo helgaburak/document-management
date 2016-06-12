d8df2c8eceba300bdefbea2f76ee7a153a3b689d
Piotr Miazek <p.miazek@gmail.com>
2016-06-12 12:00:33 +0200

    my second commit yoo

--- src/main/java/pl/com/bottega/documentmanagement/api/HelloWorld3.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/HelloWorld3.java
@@ -4,4 +4,7 @@ package pl.com.bottega.documentmanagement.api;
  * Created by Piotr on 12.06.2016.
  */
 public class HelloWorld3 {
+    public static void main(String[] args){
+        System.out.println("hello testy git-a!!");
+    }
 }
