8fe543499195f4bd7bf90ec360213445df88c7b9
piotrmiazek <p.miazek@gmail.com>
2016-06-12 11:35:26 +0200

    raz raz

--- src/main/java/pl/com/bottega/documentmanagement/api/HelloWorld.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/HelloWorld.java
@@ -4,4 +4,7 @@ package pl.com.bottega.documentmanagement.api;
  * Created by Piotr on 12.06.2016.
  */
 public class HelloWorld {
+  public static void main(String[] args){
+    system.out.println("hello mother###rs!!")
+  }
 }
