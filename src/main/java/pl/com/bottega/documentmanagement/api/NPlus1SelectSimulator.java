c742dfb99e96e64004eddf3c8b1889f0d2a72c81
Piotr Miazek <p.miazek@gmail.com>
2016-07-30 16:48:08 +0200

    30.07.2016

---
+++ src/main/java/pl/com/bottega/documentmanagement/api/NPlus1SelectSimulator.java
@@ -0,0 +1,23 @@
+package pl.com.bottega.documentmanagement.api;
+
+import java.util.UUID;
+
+/**
+ * Created by Piotr on 30.07.2016.
+ */
+public class NPlus1SelectSimulator {
+
+
+
+
+
+
+
+
+
+
+    private String randomString(){
+        return UUID.randomUUID().toString();
+    }
+
+}
